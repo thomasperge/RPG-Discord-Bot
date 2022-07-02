7897e86845e44a82ceab15a96de87aa8511e188a
Thomasperge <thomaskauffmant74@gmail.com>
2022-07-02 22:13:12 +0200

    add ping

--- commands/ping.js
+++ commands/ping.js
@@ -2,20 +2,9 @@ const Discord = require('discord.js');
 const config = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
-  var user = message.author;
-
-  message.channel.send(`Poooooong ! <:7101shrekwtf:853676911640444968>`);
-
-  const logChannel = client.channels.cache.get('584036562241585167');
-  var now = new Date();
-  var date = `${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
-  var messageEmbed = new Discord.MessageEmbed()
-    .setColor('#6BBC21')
-    .setAuthor(`Log ${date}`)
-    .setDescription(`[PING] command by: ${client.users.cache.get(user.id).username}`);
-  logChannel.send(messageEmbed);
+    message.channel.send(`Poooooong ! ❤️`);
 };
 
 module.exports.info = {
-  names: ['ping'],
+    names: ['ping'],
 };
